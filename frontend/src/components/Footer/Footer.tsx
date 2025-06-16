dc8e4346cc8527f82e40c895be4582e7b4b84eb2
James Young <jyyoung@jyymail.com>
2025-06-16 06:04:43 -0400

    fix: add link to source code in footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ const Footer = () => {
       <div className="footer-content">
         <p>METCS601 Term Project</p>
         <p>&copy; 2025 James Young</p>
+        <a className="footer-link" href="https://github.com/jamesyoung-15/METCS601-Term-Project/" target="_blank">Project Source Code</a>
       </div>
     </footer>
   );
